60c983486ecfe3a8b922b2b036331eb55112a1a2
urrdan <yarae99@gmail.com>
2019-04-27 20:19:53 -0500

    basename 2

--- src/index.js
+++ src/index.js
@@ -7,4 +7,4 @@ import ScrollToTop from './ScrollToTop'
 
 import './styles/style.css'
 
-ReactDOM.render(<Provide><BrowserRouter basename='/ecom'><ScrollToTop><App /></ScrollToTop></BrowserRouter></Provide>, document.getElementById('root'));
+ReactDOM.render(<Provide><BrowserRouter basename={process.env.PUBLIC_URL}><ScrollToTop><App /></ScrollToTop></BrowserRouter></Provide>, document.getElementById('root'));
